6a6d5ca0752446dd707a02b5c2ee8c34df2e062c
Lucas.rincondelarosa <lucasrincondelarosa@gmail.com>
2023-09-20 17:24:21 +0200

    Function for normalizing images outside dataset

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,55 @@
 import os
 import numpy as np
+import cv2
 from PIL import Image
+
+
+def normalize_images(name, channels, start=1):
+
+    path = f'data/external/2D/{name}'
+
+    norm_path = os.path.join(path, 'normalized')
+    if os.path.isdir(norm_path) and len(os.listdir(norm_path)) != 0:
+        print(f"This dataset has already been normalized")
+        return
+    
+    else:
+        if not os.path.isdir(norm_path):
+            os.makedirs(norm_path)
+
+    names = os.listdir(os.path.join(path, 'images'))
+    imgs_path = os.path.join(path, 'images')
+    paths = [os.path.join(imgs_path, img_path) for img_path in names]
+
+    if channels == 3:
+        channels = cv2.IMREAD_COLOR
+
+    elif channels == 1:
+        channels = cv2.IMREAD_GRAYSCALE
+    
+    else:
+        print("Incorrect number of channels")
+        return
+    
+    means = [np.mean(cv2.imread(path, channels), axis=(0,1)) for path in paths]
+    stds = [np.std(cv2.imread(path, channels), axis=(0,1)) for path in paths]
+    
+    means = np.array(means)
+    stds = np.array(stds)
+
+    mean = np.mean(means, axis=0)
+    std = np.mean(stds, axis=0)
+
+    for i, path in enumerate(paths, start=start):
+        img = cv2.imread(path, channels)
+        img = (img - mean) / std
+        np.save(f"{norm_path}/norm_{i}.npy", img)
+
+    return
+
+
+#####################################################################
+
 
 def processWarwick(path):
 
@@ -26,6 +75,35 @@ def processWarwick(path):
             os.system(rm)
 
     return
+
+
+#####################################################################
+
+
+def processChallenge(path, type='train'):
+
+    dsts = ['images', 'masks']
+    folders = ['01', '02']
+
+    if type == 'test':
+        dsts = ['images']
+
+    for dst in dsts:
+        if not os.path.isdir(os.path.join(path,dst)):
+            os.makedirs(os.path.join(path,dst))
+        
+        if dst == 'masks':
+            folders = ['01_ST/SEG', '02_ST/SEG']
+
+        i = 0
+        for folder in folders:
+
+            names = os.listdir(os.path.join(path, folder))
+
+            for file in names:
+                cmd = f'cp "{os.path.join(path, folder, file)}" "{os.path.join(path, dst, f"t{i}.tif")}"'
+                os.system(cmd)
+                i += 1
 
 
 #####################################################################
@@ -88,7 +166,7 @@ def processBowl(path):
 #####################################################################
 
 
-def maskBowk(path):
+def maskBowl(path):
 
     names = os.listdir(os.path.join(path,'images'))
     total = len(names)
